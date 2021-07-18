a145d1d7742529cb0a4bc935efe6add6ddc0e372
raiyan <raiyanyahyadeveloper@gmail.com>
2021-07-19 00:18:14 +0200

    fix indentation

--- api/index.py
+++ api/index.py
@@ -3,12 +3,11 @@ import os
 class handler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
-		s = self.path
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
+        s = self.path
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
         myenv = os.getenv('MYENV')
-		message = "Hello, stranger!" + myenv
-
-		self.wfile.write(message.encode())
+        message = "Hello, stranger!" + myenv
+        self.wfile.write(message.encode())
 		return
