26f49ee81502df4f251018857007eea9010e82d3
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-08-09 12:49:59 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,23 +10,17 @@ secret_key = os.getenv('SECRET')
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         c = SimpleCookie()
-        print("self is",str(self))
-        print("headers payload are ", self.headers.get_payload())
-        
-        print("secretkey is ", secret_key)
         c.load("\n".join(self.headers.get_all('cookie',failobj=[])))
-        print("the cookie is", c)
         encoded_jwt = c["__Secure-next-auth.session-token"].value
-        print("encoded secret is ", encoded_jwt)
         valid_jwt = jwt.decode(encoded_jwt, secret_key, algorithms=["HS512"])
-        print(valid_jwt)
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Credentials', 'true')
-        self.send_header('Access-Control-Allow-Origin', 'https://vercel-jwt-github-action.vercel.app')
-        self.send_header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        self.send_header('Access-Control-Allow-Headers','*')
-        self.send_header('Content-type', 'application/json')
-        
-        self.end_headers()
-        self.wfile.write(bytes("{\"result\": 200}", "utf-8"))
+        if valid_jwt:
+          self.send_response(200)
+          self.send_header('Access-Control-Allow-Credentials', 'true')
+          self.send_header('Access-Control-Allow-Origin', 'https://vercel-jwt-github-action.vercel.app')
+          self.send_header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+          self.send_header('Access-Control-Allow-Headers','*')
+          self.send_header('Content-type', 'application/json')
+          self.end_headers()
+          self.wfile.write(bytes("{\"result\": 200}", "utf-8"))
+        self.send_response(401)
         return
